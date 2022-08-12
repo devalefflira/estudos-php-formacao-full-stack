4cbb677c3b3b24d9da11016da214accd9f9fedcb
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-12 01:31:18 -0300

    Adição dos arquivos style.css e script.js

--- 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -4,12 +4,14 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>FSPHP: Iniciando um Projeto!</title>
+      <link rel="stylesheet" href="./style.css">
     </head>
     <body>
         <?php
             $start = "Vamos Começar!";
             echo "<h1>Olá Mundo! $start</h1>";
-            echo "<div id='js'>Loading...</div>";
+            echo "<p id='js'>Loading...</p>";
         ?>
+        <script src="./script.js"></script>
     </body>
 </html>
