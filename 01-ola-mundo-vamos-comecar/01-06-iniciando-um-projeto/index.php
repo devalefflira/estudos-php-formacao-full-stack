c8692396576de9e70dbbde6232a99bf085d04266
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-12 00:57:44 -0300

    Primeiro Commit

---
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>FSPHP: Iniciando um Projeto!</title>
+    </head>
+    <body>
+        <?php
+            $start = "Vamos Começar!";
+            echo "<h1>Olá Mundo! $start</h1>";
+            echo "<div id='js'>Loading...</div>";
+        ?>
+    </body>
+</html>
